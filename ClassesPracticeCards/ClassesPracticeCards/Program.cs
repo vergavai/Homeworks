8b0d6bca849c80d7f8c0fd32710ac0d597ab057e
Nikolay Klitovchenko <148769228+vergavai@users.noreply.github.com>
2026-02-16 14:40:15 +0300

    OOP practice

---
+++ ClassesPracticeCards/ClassesPracticeCards/Program.cs
@@ -0,0 +1,184 @@
+﻿namespace ClassesPracticeCards;
+
+interface ICard
+{
+    public string Name { get; }
+}
+
+interface IPuller
+{
+    public void PullCard(ICard card, IPlayerDeck playerDeck);
+}
+
+interface ICardShower
+{
+    public void ShowCards();
+}
+
+interface IPlayerDeck : ICardShower
+{
+    public IReadOnlyList<ICard> Cards { get; }
+    public void AddCard(ICard card);
+}
+
+interface IDeck : IPuller, ICardShower
+{
+    public IReadOnlyList<ICard> Cards { get; }
+}
+
+interface IPlayer
+{
+    public IPlayerDeck Deck { get; }
+}
+
+class Card : ICard
+{
+    public string Name => _name;
+
+    private readonly string _name;
+
+    public Card(string name)
+    {
+        _name = name;
+    }
+}
+
+class Deck : IDeck
+{
+    public IReadOnlyList<ICard> Cards => _cards;
+
+    private readonly List<ICard> _cards;
+
+    public Deck(List<ICard> cards)
+    {
+        _cards = cards;
+    }
+
+    public void PullCard(ICard card, IPlayerDeck playerDeck)
+    {
+        playerDeck.AddCard(card);
+        _cards.Remove(card);
+    }
+
+    public void ShowCards()
+    {
+        foreach (var card in _cards)
+        {
+            Console.Write(card.Name + " ");
+        }
+
+        Console.WriteLine();
+    }
+}
+
+class PlayerDeck : IPlayerDeck
+{
+    public IReadOnlyList<ICard> Cards => _cards;
+
+    private readonly List<ICard> _cards;
+
+    public PlayerDeck(List<ICard> cards)
+    {
+        _cards = cards;
+    }
+
+    public void AddCard(ICard card)
+    {
+        _cards.Add(card);
+    }
+
+    public void ShowCards()
+    {
+        foreach (var card in _cards)
+        {
+            Console.Write(card.Name + " ");
+        }
+
+        Console.WriteLine();
+    }
+}
+
+class Player : IPlayer
+{
+    public IPlayerDeck Deck => _playerDeck;
+
+    private readonly IPlayerDeck _playerDeck;
+
+    public Player(IPlayerDeck playerDeck)
+    {
+        _playerDeck = playerDeck;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string inputString;
+        int inputInt;
+        
+        IDeck deck = new Deck
+        (new List<ICard>
+        {
+            new Card("Tuz"),
+            new Card("Gaz"),
+            new Card("Pika"),
+            new Card("Den"),
+            new Card("Bubna"),
+            new Card("Dota"),
+            new Card("War"),
+            new Card("Beb"),
+        });
+
+        IPlayerDeck playerDeck = new PlayerDeck(new List<ICard>());
+
+        IPlayer player = new Player(playerDeck);
+
+        Console.WriteLine(deck.Cards.Count);
+
+        while (true)
+        {
+            if (deck.Cards.Count == 0)
+            {
+                Console.WriteLine("Вы забрали все карты");
+                playerDeck.ShowCards();
+                break;
+            }
+            
+            Console.Write("Ваш выбор: ");
+            inputString = Console.ReadLine();
+
+            if (inputString == "pull")
+            {
+                Console.Write("Сколько кард взять?: ");
+                inputInt = int.Parse(Console.ReadLine());
+
+                if (inputInt > deck.Cards.Count)
+                {
+                    Console.WriteLine("Вы написали число больше, чем есть кард, вместо этого вы берете все карты");
+                    inputInt = deck.Cards.Count;
+                }
+
+                for (int i = 0; i < inputInt; i++)
+                {
+                    deck.PullCard(deck.Cards[0], playerDeck);
+                }
+                
+            }
+
+            else if(inputString == "show")
+            {
+                Console.WriteLine("Ваши карты: ");
+                playerDeck.ShowCards();
+                Console.WriteLine("Карты в колоде: ");
+                deck.ShowCards();
+            }
+            
+            
+
+        }
+        
+
+
+    }
+}
---
+++ ClassesPracticeShop/ClassesPracticeShop/Program.cs
@@ -0,0 +1,148 @@
+﻿interface ITrader : ISeller
+{
+    IReadOnlyList<IProduct> ProductsInStock { get; }
+}
+
+interface ISeller
+{
+    void Sell(IProduct product);
+}
+
+interface IProduct
+{
+    string Name { get; }
+}
+
+interface IPlayer
+{
+    IInventory Inventory { get; }
+}
+
+interface IInventory
+{
+    IReadOnlyList<IItem> Items { get; }
+    void AddItem(IItem item);
+}
+
+interface IItem
+{
+    string Name { get; }
+}
+
+class Trader : ITrader
+{
+    public IReadOnlyList<IProduct> ProductsInStock => _productsInStock;
+
+    private readonly List<IProduct> _productsInStock;
+    private readonly IInventory _inventory;
+
+    public Trader(List<IProduct> productsInStock, IInventory inventory)
+    {
+        _productsInStock = productsInStock;
+        _inventory = inventory;
+    }
+
+    public void Sell(IProduct product)
+    {
+        _inventory.AddItem(new Item(product.Name));
+        _productsInStock.Remove(product);
+    }
+}
+
+class Product : IProduct
+{
+    public string Name => _name;
+
+    private readonly string _name;
+
+    public Product(string name)
+    {
+        _name = name;
+    }
+}
+
+class Player : IPlayer
+{
+    public IInventory Inventory => _inventory;
+
+    private readonly IInventory _inventory;
+
+    public Player(IInventory inventory)
+    {
+        _inventory = inventory;
+    }
+}
+
+class Inventory : IInventory
+{
+    public IReadOnlyList<IItem> Items => _items;
+
+    private readonly List<IItem> _items;
+
+    public Inventory(List<IItem> items)
+    {
+        _items = items;
+    }
+
+    public void AddItem(IItem item)
+    {
+        _items.Add(item);
+    }
+} 
+
+class Item : IItem
+{
+    public string Name => _name;
+
+    private readonly string _name;
+
+    public Item(string name)
+    {
+        _name = name;
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        IInventory inventory = new Inventory(new List<IItem>());
+
+        List<IProduct> productsInStock = new List<IProduct>
+        {
+            new Product("Balalaika"),
+            new Product("Xui")
+        };
+        
+        ITrader trader = new Trader(productsInStock, inventory);
+        
+        Console.WriteLine("Trader stock before sale:");
+        
+        foreach (var product in trader.ProductsInStock)
+        {
+            Console.WriteLine(product.Name);
+        }
+        
+        Console.WriteLine();
+
+        IPlayer player = new Player(inventory);
+        
+        trader.Sell(trader.ProductsInStock[1]);
+
+        Console.WriteLine("Trader stock after sale:");
+        
+        foreach (var product in trader.ProductsInStock)
+        {
+            Console.WriteLine(product.Name);
+        }
+        
+        Console.WriteLine();
+        
+        Console.WriteLine("Player Inventory:");
+        
+        foreach (var item in player.Inventory.Items)
+        {
+            Console.WriteLine(item.Name);
+        }
+    }
+}
